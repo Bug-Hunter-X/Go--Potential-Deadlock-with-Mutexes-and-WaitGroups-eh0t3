a1003ae93fffefe13ba5ccd16cb4962b98b80a81
BugHunter-X <askzenai@gmail.com>
2025-01-23 07:46:06 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,26 @@
+```go
+package main
+
+import (
+
+        "fmt"
+        "sync"
+)
+
+func main() {
+        var wg sync.WaitGroup
+        var m sync.Mutex
+        for i := 0; i < 10; i++ {
+                wg.Add(1)
+                go func(i int) {
+                        defer wg.Done()
+                        m.Lock()
+                        fmt.Printf("Goroutine %d is working...\n", i)
+                        // Simulate some work
+                        //time.Sleep(time.Millisecond * 100)
+                        m.Unlock()
+                }(i)
+        }
+        wg.Wait()
+}
+```
